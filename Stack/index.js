d0be2d5a2b9b546a5b506e3a96c5be000fb3b905
ashwin7mak <m.ashwinkumar7@gmail.com>
2019-04-06 18:26:42 -0700

    Stack implementation

---
+++ Stack/index.js
@@ -0,0 +1,64 @@
+// Stack Class
+class Stack {
+    // Array implementation
+    constructor() {
+        this.item = [];
+    }
+
+    // Push element
+    push(element) {
+        this.item.push(element);
+    }
+
+    // Pop element
+    pop() {
+        // Check if the length of stack is greater than 0
+        // If not return underflow
+        if (this.item.length < 0) {
+            return "Underflow";
+        } else {
+            return this.item.pop();
+        }
+    }
+
+    peek() {
+        // Return the last element in the array
+        console.log("Peek element is", this.item[this.item.length - 1]);
+        return this.item[this.item.length - 1];
+    }
+
+    isEmpty() {
+        return this.item.length === 0;
+    }
+
+    printStack() {
+        let res = '';
+        for (let i = 0; i < this.item.length; i++) {
+            res += this.item[i] + "-"
+        }
+        console.log("--->", res);
+    }
+}
+
+
+let stack1 = new Stack();
+
+stack1.push(10);
+stack1.push(20);
+stack1.push(30);
+stack1.push(40);
+stack1.push(50);
+
+stack1.printStack();
+
+stack1.peek();
+
+stack1.push(100);
+stack1.printStack();
+
+stack1.pop();
+stack1.printStack();
+
+
+stack1.isEmpty();
+stack1.printStack();
